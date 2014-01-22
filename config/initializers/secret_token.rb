8eae4f62cbeb6c7e0acc81fe43c270a5470f9c1a
Stefan Vignir <stefanvignir@stefanvignir.is>
2014-01-22 19:29:43 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fluttershy::Application.config.secret_key_base = '0e33789b8238e4c8819fcccfd2ef11b54a0654b8536084c7c673b3c587cc0c2e2be3273c3ba60a5bc321512366e7c2e891b7eb9cba1932836ce07ce62ed62571'
