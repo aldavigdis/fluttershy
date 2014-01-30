2ad7279f307e164f4da99d8de55edd10fb28ac93
Stefan Vignir <stefanvignir@stefanvignir.is>
2014-01-30 15:28:30 +0000

    Did some tidying up and fixed some minor bugs

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,4 @@
 class Company < ActiveRecord::Base
+  validates :name, presence: { :message => "cannot be blank" }
+  validates :kt, format: { with: /\A[0-9]+\z/, message: "Only numbers allowed" }, :allow_blank => true
 end
