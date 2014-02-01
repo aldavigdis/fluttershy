bad974252fd4598f75082b1ea67f512121d98447
Stefan Vignir <stefanvignir@stefanvignir.is>
2014-02-01 11:07:47 +0000

    Added index view it Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+  
+  def index
+    @company = Company.find(params[:company_id])
+  end
+  
+  def new
+    @company = Company.find(params[:company_id])
+  end
+  
 end
