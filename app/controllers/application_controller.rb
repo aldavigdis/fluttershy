0b38fb92c5634d3382cad98d6cb0c5ae4cf8b4d9
Stefan Vignir <stefanvignir@stefanvignir.is>
2014-02-03 02:32:07 +0000

    Added 5 user access levels to the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def user_access_levels
+    return { User: 0, Kiosk: 1, Admin: 2, Superuser: 3, Superadmin: 4 }
+  end
+    
 end
