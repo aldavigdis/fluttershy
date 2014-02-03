b2dad4e0dacea956ac7b0016905c0164cb252ee7
Stefan Vignir <stefanvignir@stefanvignir.is>
2014-02-03 13:43:04 +0000

    Added methods for user login and registration to the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,17 @@ class ApplicationController < ActionController::Base
   def user_access_levels
     return { User: 0, Kiosk: 1, Admin: 2, Superuser: 3, Superadmin: 4 }
   end
+  
+  def password_seed
+    return SecureRandom.hex(n=32).to_i(16).to_s(36)
+  end
+  
+  def password_hash(password, seed)
+    return Digest::Whirlpool.hexdigest(password+seed)
+  end
+  
+  def login_startpath(access)
+    return companies_path
+  end
     
 end
