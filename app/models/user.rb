fbc1414cee7565d1c97397ccddec433b59e46d93
Stefan Vignir <stefanvignir@stefanvignir.is>
2014-02-05 07:57:53 +0000

    Added validations to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
+  
   belongs_to :company
   has_many :logins
+  
+  validates :fullname, presence: { :message => "cannot be blank" }
+  validates :email, format: { with: /@/, message: "must be a valid email address" }
+  validates :password, presence: { :message => "cannot be blank" }
+  
 end
