2ea53d1e314bd1a1c1d0e58915e5356c0c0e66db
Stefan Vignir <stefanvignir@stefanvignir.is>
2014-02-06 13:30:20 +0000

    Changed the user model so logins will be destroyed on delete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ActiveRecord::Base
   
   belongs_to :company
-  has_many :logins
+  has_many :logins, :dependent => :destroy
   
   validates :fullname, presence: { :message => "cannot be blank" }
   validates :email, format: { with: /@/, message: "must be a valid email address" }
-  validates :password, presence: { :message => "cannot be blank" }
   
 end
