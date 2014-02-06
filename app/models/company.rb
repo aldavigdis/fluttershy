4c690de8dffeeb835af94a6d69131f214315aabb
Stefan Vignir <stefanvignir@stefanvignir.is>
2014-02-06 13:29:37 +0000

    Changed the cmpany model so users will be destroyed on delete

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,6 @@
 class Company < ActiveRecord::Base
   
-  has_many :users
+  has_many :users, :dependent => :destroy
     
   validates :name, presence: { :message => "cannot be blank" }
   validates :kt, format: { with: /\A[0-9]+\z/, message: "Only numbers allowed" }, :allow_blank => true
