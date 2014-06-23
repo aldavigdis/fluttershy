36337e23966b0ce41a4521299a2e769be8a1d993
Stefan Vignir <stefanvignir@stefanvignir.is>
2014-06-23 20:44:45 +0000

    Cleaned up nav_item and added a separate nav_item_sidebar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,16 +4,40 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   helper_method :nav_item
+  helper_method :nav_item_sidebar
   
-  def nav_item(text, url, controller=nil)
-    if params[:controller] == controller
+  def nav_item(text, url, controller=nil, forceActive=nil)
+    if forceActive == true
       css_class = 'active'
+    else
+      
+      if params[:controller] == controller
+        css_class = 'active'
+      end
+      if controller == 'companies'
+        if params[:company_id] && params[:company_id].to_i ==
+          Flutter::CurrentUser.company
+            css_class = ''
+        else
+          css_class = 'active'
+        end
+      elsif controller == 'users'
+        if params[:company_id] && params[:company_id].to_i ==
+          Flutter::CurrentUser.company
+            css_class = 'active'
+        else
+          css_class = ''
+        end
+      end
+      
     end
-    return '<li class="'+css_class.to_s+'"><a href="'+url.to_s+'">'+text.to_s+'</a></li>'
-  end
   
   def user_access_levels
     return { User: 0, Kiosk: 1, Admin: 2, Superuser: 3, Superadmin: 4 }
+    
+    return %(
+      <li class="#{css_class}"><a href="#{url.to_s}">#{text.to_s}</a></li>
+    )
   end
   
   def user_access_levels_available(access)
@@ -25,9 +49,18 @@ class ApplicationController < ActionController::Base
         end
       end
       return output_hash
-    else
-      return {}
+  def nav_item_sidebar(text, url, controller=nil, active=nil, icon=nil)
+    if active == true
+      css_class = 'active'
     end
+    
+    if icon
+      link_string = %(<li class="#{css_class}"><a href="#{url.to_s}"><span class="glyphicon glyphicon-#{icon} pull-right"></span>#{text.to_s}</a></li>)
+    else
+      link_string = %(<li class="#{css_class}"><a href="#{url.to_s}">#{text.to_s}</a></li>)
+    end
+    
+    return link_string
   end
   
   def password_seed
