ba1579dc21070b724a12963f01ee253c215ff2a5
Stefan Vignir <stefanvignir@stefanvignir.is>
2014-06-23 19:47:09 +0000

    Added to and updated documentation for login controller

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,7 +1,14 @@
+# Public: The login controller.
 class LoginController < ApplicationController
   
   layout "login", only: [:index]
   
+  # Public: Register login attempt in the database.
+  #
+  # user_id - The user's ID
+  # success - Boolean value for login success or failure
+  #
+  # Returns true on success, false on falure.
   def login_register(user_id, success)
     ip_addr = IPAddr.new(request.remote_ip).to_i
     useragent = request.env['HTTP_USER_AGENT']
@@ -12,6 +19,10 @@ class LoginController < ApplicationController
     return this_login.save
   end
   
+  # Public: Log the user out
+  #
+  # Log the user out by clearing cookies and session data, and then redirect
+  # to the root.
   def logout
     reset_session
     cookies.permanent.signed[:remember_user_id] = nil
@@ -19,13 +30,14 @@ class LoginController < ApplicationController
     redirect_to root_path
   end
   
+  # Public: Process requests and render the main view
   def index
     
     # Check if user is already logged in
     if session[:user_id]
       redirect_to login_startpath(session[:user_access])
       
-    # Check if user is logging in right now
+    # If not, check if user is logging in right now
     elsif params[:email] && params[:password]
       sleep 1
       check_user = User.find_by(email: params[:email])
@@ -46,7 +58,7 @@ class LoginController < ApplicationController
         sleep 2
       end
     
-    # Check if user has a saved session
+    # If not, check if user has a saved session
     elsif cookies.permanent.signed[:remember_user_id] && cookies.permanent.signed[:remember_user_token]
       check_user = User.find(cookies.permanent.signed[:remember_user_id])
       check_hash = Digest::Whirlpool.hexdigest(cookies.permanent.signed[:remember_user_token] + check_user.password_seed)
@@ -60,6 +72,7 @@ class LoginController < ApplicationController
       end
     end
     
+    # Proceed with the login if everything checks out
     if login_ok === true
       
       # Register the login attempt
@@ -72,14 +85,18 @@ class LoginController < ApplicationController
       session[:user_access] = login_user.access
       session[:company_id] = login_user.company_id
       
-      # Check if the user wanted the session to be saved
+      # Check if the user wanted the session to be saved.
       if params[:remember] == true
-        # The password_seed method is used to generate the stored token
+        
+        # The password_seed method is used to generate the stored token.
+        # The remember_token is then stored as a signed cookie along with the
+        # user's ID from the database.
         remember_token = password_seed
-        # The token is stored as a signed cookie
         cookies.permanent.signed[:remember_user_id] = login_user.id
         cookies.permanent.signed[:remember_user_token] = remember_token
-        # The WHIRLPOOL hash of password_seed and the user's password seed is stored in database
+        
+        # The WHIRLPOOL hash of remember_token and the user's password seed is
+        # then stored in the database.
         remember_hash = Digest::Whirlpool.hexdigest(remember_token+login_user.password_seed)
         login_user.update_attributes(:remember_hash => remember_hash)
         login_user.save
