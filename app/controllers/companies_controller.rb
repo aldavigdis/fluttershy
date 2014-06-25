9ea9b9511db5e414603e3f33ad71bff808016e44
Stefan Vignir <stefanvignir@stefanvignir.is>
2014-06-25 18:20:37 +0000

    Huge update/cleanup for the companies controller

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -3,15 +3,23 @@ class CompaniesController < ApplicationController
   require "Flutter"
   
   def index
-    if session[:user_access] == 3 || session[:user_access] == 4
+    Flutter::CurrentUser.init_data({access: session[:user_access]})
+    if Flutter::CurrentUser.is_least_superuser
       if params[:search]
         if params[:search].present?
           @companies = Company.search(name: params[:search])
+          respond_to do |format|
+            format.html
+            format.json { render json: @companies }
+          end
         else
           @companies = {}
         end
       else
-        @companies = Company.paginate(page: params[:page])
+        respond_to do |format|
+          format.html { @companies = Company.paginate(page: params[:page]) }
+          format.json { @companies = Company.all; render json: @companies }
+        end
       end
     else
       access_denied
@@ -19,15 +27,17 @@ class CompaniesController < ApplicationController
   end
   
   def new
-    @company = Company.new
-    if session[:user_access] == 4
+    Flutter::CurrentUser.init_data({access: session[:user_access]})
+    if Flutter::CurrentUser.is_superadmin
+      @company = Company.new
     else
       access_denied
     end
   end
   
   def create
-    if session[:user_access] == 4
+    Flutter::CurrentUser.init_data({access: session[:user_access]})
+    if Flutter::CurrentUser.is_superadmin
       @company = Company.new(company_params)
       if @company.save
         redirect_to @company
@@ -40,39 +50,69 @@ class CompaniesController < ApplicationController
   end
   
   def show
+    Flutter::CurrentUser.init_data({
+      access: session[:user_access], company: session[:company_id]
+    })
     @company = Company.find(params[:id])
-    @company_has_billing_address = false
-    @company_has_shipping_address = false
-    
-    if @company.address1.present? || @company.address2.present? || @company.postcode.present? || @company.city.present?
-      @company_has_billing_address = true
-    end
-    
-    if @company.shipping_address1.present? || @company.shipping_address2.present? || @company.shipping_postcode.present? || @company.shipping_city.present?
-      @company_has_shipping_address = true
-    end
-    
-  end
-  
-  def edit
-    @company = Company.find(params[:id])
-    if (session[:user_access] == 2 && session[:company_id] == @company.id) || session[:user_access] == 4
+
+    if Flutter::CurrentUser.is_least_superuser ||
+      (Flutter::CurrentUser.is_least_user && Flutter::CurrentUser.company ==
+      @company.id)
+
+      @company_has_billing_address = false
+      @company_has_shipping_address = false
+
+      @company_has_billing_address = @company.address1.present? ||
+        @company.address2.present? || @company.postcode.present? ||
+        @company.city.present?
+
+      @company_has_shipping_address = @company.shipping_address1.present? ||
+        @company.shipping_address2.present? ||
+        @company.shipping_postcode.present? || @company.shipping_city.present?
+      
+      respond_to do |format|
+        format.html
+        format.json { render json: @company }
+      end
+      
     else
       access_denied
     end
   end
   
-  def update
+  def edit
+    Flutter::CurrentUser.init_data({
+      access: session[:user_access], company: session[:company_id]
+    })
     @company = Company.find(params[:id])
-    if session[:user_access] == 4
-      access_ok = true
-      company_update = @company.update(params[:company].permit(:name, :kt, :email, :tel, :mobile, :fax, :contact_name, :comments, :address1, :address2, :postcode, :city, :shipping_address1, :shipping_address2, :shipping_postcode, :shipping_city, :enabled, :comments))
-    elsif session[:user_access] == 2 && session[:company_id] == @company.id
-      access_ok = true
-      company_update = @company.update(params[:company].permit(:email, :tel, :mobile, :fax, :contact_name, :comments, :address1, :address2, :postcode, :city, :shipping_address1, :shipping_address2, :shipping_postcode, :shipping_city))
-    end
     
-    if access_ok
+    if !(Flutter::CurrentUser.is_superadmin || (Flutter::CurrentUser.is_admin &&
+      Flutter::CurrentUser.company == @company.id))
+      access_denied
+    end
+  end
+  
+  def update
+    Flutter::CurrentUser.init_data({
+      access: session[:user_access], company: session[:company_id]
+    })
+    @company = Company.find(params[:id])
+    
+    if Flutter::CurrentUser.is_superadmin || (Flutter::CurrentUser.is_admin &&
+      Flutter::CurrentUser.company == @company.id)
+      if Flutter::CurrentUser.is_superadmin
+        company_update = @company.update(params[:company].permit(
+          :name, :kt, :email, :tel, :mobile, :fax, :contact_name, :comments,
+          :address1, :address2, :postcode, :city, :shipping_address1,
+          :shipping_address2, :shipping_postcode, :shipping_city, :enabled,
+          :comments))
+      elsif Flutter::CurrentUser.is_admin && Flutter::CurrentUser.company
+        company_update = @company.update(params[:company].permit(
+          :email, :tel, :mobile, :fax, :contact_name, :comments, :address1,
+          :address2, :postcode, :city, :shipping_address1, :shipping_address2,
+          :shipping_postcode, :shipping_city))
+      end
+      
       if company_update
         redirect_to @company
       else
@@ -85,12 +125,14 @@ class CompaniesController < ApplicationController
   end
   
   def destroy
+    Flutter::CurrentUser.init_data({
+      access: session[:user_access],
+      company: session[:company_id]
+    })
     @company = Company.find(params[:id])
-    if session[:user_access] == 4
-      if @company.id == session[:company_id]
-        @companies = Company.all
-        @error = "You cannot erase your own company"
-        redirect_to companies_path
+    if Flutter::CurrentUser.is_superadmin
+      if Flutter::CurrentUser.company == @company.id
+        access_denied
       else
         @company.destroy 
         redirect_to companies_path
@@ -103,24 +145,9 @@ class CompaniesController < ApplicationController
   private
   def company_params
     params.require(:company).permit(
-    :name,
-    :kt,
-    :email,
-    :tel,
-    :mobile,
-    :fax,
-    :contact_name,
-    :comments,
-    :address1,
-    :address2,
-    :postcode,
-    :city,
-    :shipping_address1,
-    :shipping_address2,
-    :shipping_postcode,
-    :shipping_city,
-    :comments,
-    :enabled
-    )
+      :name, :kt, :email, :tel, :mobile, :fax, :contact_name, :comments,
+      :address1, :address2, :postcode, :city, :shipping_address1,
+      :shipping_address2, :shipping_postcode, :shipping_city, :comments,
+      :enabled)
   end
 end
